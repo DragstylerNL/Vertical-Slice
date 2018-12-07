3338b14c759d7dedc58a2a2b043227f2ba014edd
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-12-07 14:33:57 +0100

    End turn Button

---
+++ verticalSlice1/Assets/Private Folders/Peter/Scripts/EndTurnButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurnButton : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool endTurn = true;
+
+    private float currentRotation = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        TurnButton();
+        Debug.Log("Clicked");
+    }
+
+    public void TurnButton()
+    {
+        if (endTurn)
+        {
+            //while (transform.rotation.eulerAngles.x < 180)
+            transform.Rotate(Vector3.right * Time.deltaTime);
+
+            Debug.Log(transform.rotation.eulerAngles.x);
+        }
+    }
+}
