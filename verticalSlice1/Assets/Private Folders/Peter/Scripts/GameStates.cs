43b57fb0748ead2cf6d3970daa38702970ef0cee
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-12-10 11:51:55 +0100

    Kaartje erin gezet - Final Card

--- verticalSlice1/Assets/Private Folders/Peter/Scripts/GameStates.cs
+++ verticalSlice1/Assets/Private Folders/Peter/Scripts/GameStates.cs
@@ -9,12 +9,19 @@ public class GameStates : MonoBehaviour
     [SerializeField]
     private gameStates currentGameState = gameStates.Playing;
 
-
+    /// <summary>
+    /// Gets the current Game State
+    /// </summary>
+    /// <returns></returns>
     public gameStates getGameStates()
     {
         return currentGameState;
     }
 
+    /// <summary>
+    /// Sets the current Game State
+    /// </summary>
+    /// <param name="_value">New game state</param>
     public void setGameStates(gameStates _value)
     {
         currentGameState = _value;
