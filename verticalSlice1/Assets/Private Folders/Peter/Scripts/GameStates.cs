1c66b6e0fe01f66f153fb4166b4daaa1239f5dec
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-12-10 11:20:34 +0100

    Begonnen: Game states

---
+++ verticalSlice1/Assets/Private Folders/Peter/Scripts/GameStates.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+    public enum gameStates { Playing, EndTurn }
+
+    [SerializeField]
+    private gameStates currentGameState = gameStates.Playing;
+
+
+    public gameStates getGameStates()
+    {
+        return currentGameState;
+    }
+
+    public void setGameStates(gameStates _value)
+    {
+        currentGameState = _value;
+    }
+
+}
