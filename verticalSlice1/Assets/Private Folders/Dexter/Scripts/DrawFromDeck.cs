e5fe1e9df1686ec74db04a929448254c2aaa3579
Dexter <itsdexter66@gmail.com>
2018-12-12 12:02:56 +0100

    Begin Draw deck card animation

---
+++ verticalSlice1/Assets/Private Folders/Dexter/Scripts/DrawFromDeck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawFromDeck : MonoBehaviour 
+{
+    [SerializeField]
+    private List<Vector3> positionsList;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        positionsList.Add(GameObject.Find("PlayerDeck").transform.position);//Start
+        positionsList.Add(GameObject.Find("PlayerShowCard").transform.position);//Mid
+        positionsList.Add(GameObject.Find("PlayerHand").transform.position);//End
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if (Input.GetKeyDown("Space"))
+        {
+            StartCoroutine(PlayAnimation(2f));
+        }
+    }
+
+    private IEnumerator PlayAnimation(float waitTime)
+    {
+
+        yield return new WaitForSeconds(waitTime);
+    }
+}
