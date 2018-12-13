073b6783989d34d1aa93ceb91c2f04b74c88c662
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2018-12-13 14:12:46 +0100

    vs2

---
+++ verticalSlice1/Assets/simpleAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleAnimation : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            MovetoExample();
+        }
+    }
+    void MovetoExample()
+    {
+        iTween.MoveTo(this.gameObject, iTween.Hash("position", new Vector3(-6.46f, 0.42f, 1.582f), "time", 1f, "easeType", iTween.EaseType.easeInOutSine));
+    }
+}
