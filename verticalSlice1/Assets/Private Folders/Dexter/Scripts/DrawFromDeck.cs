101cc1bcbc652fb907bbbdce697a0efa22767237
Dexter <itsdexter66@gmail.com>
2018-12-13 12:02:10 +0100

    Card animation changes

--- verticalSlice1/Assets/Private Folders/Dexter/Scripts/DrawFromDeck.cs
+++ verticalSlice1/Assets/Private Folders/Dexter/Scripts/DrawFromDeck.cs
@@ -19,7 +19,7 @@ public class DrawFromDeck : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-		if (Input.GetKeyDown("Space"))
+		if (Input.GetKeyDown("space"))
         {
             StartCoroutine(PlayAnimation(2f));
         }
@@ -27,6 +27,27 @@ public class DrawFromDeck : MonoBehaviour
 
     private IEnumerator PlayAnimation(float waitTime)
     {
+        var speed = 10;
+
+
+
+        while (transform.localPosition != positionsList[1])
+        {
+            var step = speed * Time.deltaTime;
+            //Stage 1: Deck to show
+            transform.localPosition = Vector3.MoveTowards(transform.position, positionsList[1], step);
+            Debug.Log("Moving");
+        }
+
+        yield return new WaitForSeconds(waitTime);
+
+        while (transform.localPosition != positionsList[2])
+        {
+            var step = speed * Time.deltaTime;
+            //Stage 1: Deck to show
+            transform.localPosition = Vector3.MoveTowards(transform.position, positionsList[2], step);
+            Debug.Log("Moving");
+        }
 
         yield return new WaitForSeconds(waitTime);
     }
