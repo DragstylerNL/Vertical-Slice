2c0ad8c677c412ad0df2e92f5ddeb8aab7514461
PeterSchreuderMA <24441@ma-web.nl>
2018-12-13 22:51:07 +0100

    Begin Card Attack
    Aangemaakt en aangepast:
    - CardDrag_Peter
    - CardPlace_Peter
    - SetCardStats_Peter
    
    Card object veranderd:
    - Useless objects verwijderd
    - Alles een goede naam gegeven
    - Text een outline gegeven

---
+++ verticalSlice1/Assets/Private Folders/Peter/Prefabs/Card/Scripts/CardPlace_Peter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlace_Peter : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> disappearList;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void PlaceCard()
+    {
+
+    }
+}
---
+++ verticalSlice1/Assets/Private Folders/Peter/Prefabs/Card/Scripts/SetCardStats_Peter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetCardStats_Peter : MonoBehaviour
+{
+
+    // the card we need to subract stats from
+    public Card cardSt;
+
+    // the object we need to adjust to the card's stats
+    public Text mana, attack, health, name, description;
+
+    void Start ()
+    {
+        mana.text = cardSt.Mana.ToString();
+
+        attack.text = cardSt.Attack.ToString();
+
+        health.text = cardSt.Health.ToString();
+
+        name.text = cardSt.Name;
+        description.text = cardSt.Description;
+    }
+}
