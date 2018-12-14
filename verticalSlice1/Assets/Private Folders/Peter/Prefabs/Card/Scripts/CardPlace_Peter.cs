249e4b516e951b65847f0dfd95777ffa9b645127
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-12-14 19:53:32 +0100

    Card place tween

--- verticalSlice1/Assets/Private Folders/Peter/Prefabs/Card/Scripts/CardPlace_Peter.cs
+++ verticalSlice1/Assets/Private Folders/Peter/Prefabs/Card/Scripts/CardPlace_Peter.cs
@@ -16,12 +16,14 @@ public class CardPlace_Peter : MonoBehaviour
     private Vector3 ahPositionStart, ahPositionPlaced;//Attack & Health position
     private float ahDistanceStart, ahDistancePlaced;//Attack & Health horizontal distance from the ahPosition
 
-
+    private Vector3 startRotation;
 
     // Use this for initialization
     void Start ()
     {
         placedPos = new Vector3(0, .15f, -.9f);
+
+        startRotation = new Vector3(-90, 180, 0);
 
         ahPositionStart = new Vector3(0, -854, 0.121f);
         ahPositionPlaced = new Vector3(0, -163, 0.064f);
@@ -35,10 +37,9 @@ public class CardPlace_Peter : MonoBehaviour
     {
         DisappearObjects();
         SetUpPlacedCard();
-        iTween.MoveTo(gameObject, iTween.Hash("position", placedPos, "time", .5f, "easeType", iTween.EaseType.easeInSine));
-        //iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(90, 0, 0), "easeType", "easeInOutBack"));
+        
         //transform.position = placedPos;
-
+        StartCoroutine(AnimationPlaceCard());
         //Run the attack script
         //GetComponent<CardAttack_Peter>().CanAttack(true);
     }
@@ -66,5 +67,24 @@ public class CardPlace_Peter : MonoBehaviour
         //Move the AH icons to the right place
         attackObj.transform.localPosition = new Vector3(ahDistancePlaced,0,0);
         healthObj.transform.localPosition = new Vector3(-ahDistancePlaced, 0, 0);
+    }
+
+    private IEnumerator AnimationPlaceCard()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("position", placedPos, "time", .5f, "easeType", iTween.EaseType.easeInSine));
+
+        iTween.RotateTo(gameObject, iTween.Hash("rotation", new Vector3(-71, 216, -35), "time", .5f, "easeType", "easeInOutBack", "oncomplete", "goBackToOriginal"));
+
+        
+
+        yield return new WaitForSeconds(1f);
+
+        
+
+    }
+
+    private void goBackToOriginal()
+    {
+        iTween.RotateTo(gameObject, iTween.Hash("rotation", startRotation, "time", .5f, "easeType", "easeInOutBack"));
     }
 }
