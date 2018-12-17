e1466e2d34d7036558b54208ef3888da3a0e4700
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2018-12-17 09:36:13 +0100

    TurnHistory

---
+++ verticalSlice1/Assets/Private Folders/Martijn/Scripts/TurnHistoryIcon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TurnHistoryIcon : MonoBehaviour
+{
+	public void CardHistory ()
+	{ 
+	    iTween.MoveTo(this.gameObject, iTween.Hash("position", new Vector3(-6.46f, 0.42f, 1.582f), "time", 1f, "easeType", iTween.EaseType.easeInOutSine));
+	}
+}
