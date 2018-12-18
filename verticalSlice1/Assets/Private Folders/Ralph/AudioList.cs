eba79634eb5e1dbdb1c8438be4e5a25cd779bf45
DragstylerNL <dragstylernl@gmail.com>
2018-12-18 12:27:33 +0100

    start on sound mnger

---
+++ verticalSlice1/Assets/Private Folders/Ralph/AudioList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioList : MonoBehaviour {
+    
+    public List<AudioClip> mList = new List<AudioClip>();
+
+    /*  AudioList Numbers = WhatSound
+     *      0 BOAR - ENTER
+     *      1 BOAR - ATTACK
+     *      2
+     *      3
+     *      4
+     *      5
+     *      6
+     *      
+     */
+     
+}
---
+++ verticalSlice1/Assets/Private Folders/Ralph/AudioSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSystem : MonoBehaviour {
+
+    // refrence to the list
+    private AudioList mList;
+    // refrence to the audioOutput
+    private AudioSource audioS;
+
+    void Start()
+    {
+        // get the AudioSource
+        audioS = GetComponent<AudioSource>();
+        // get the list
+        mList = GetComponent<AudioList>();
+
+        PlayThisSound(0);
+    }
+
+    public void PlayThisSound(int songNumber)
+    {
+        audioS.clip = mList.mList[songNumber];
+        audioS.Play();
+    }
+}
